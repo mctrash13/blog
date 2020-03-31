febf3e9e4dddf37ceeb4545f2b9dfc9941f9a135
mctrash13 <mctrash13@gmail.com>
2020-03-31 22:28:55 +0300

    postgres

--- untitled/settings.py
+++ untitled/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'untitled.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql_psycopg2',
+'NAME': 'myblog',
+'USER': 'myblog',
+'PASSWORD': 'q123456Q',
+'HOST': 'localhost',
+'PORT': '',
+}
 }
 
 
